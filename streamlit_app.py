717367ee70a0cdda3aabdf71f7b35649e7bd408f
aufafreshikaaryani07-jpg <aufafreshikaaryani07@gmail.com>
2026-06-05 10:40:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -415,11 +415,15 @@ elif menu == "Kalkulator pH":
         ph = round(hasil, 2)
 
         # Klasifikasi pH
-        if ph < 7:
-            sifat = "Asam"
-        elif ph == 7:
-            sifat = "Netral"
-        else:
-            sifat = "Basa"
+        if ph < 3:
+    sifat = "Asam Kuat"
+elif ph < 7:
+    sifat = "Asam Lemah"
+elif ph == 7:
+    sifat = "Netral"
+elif ph <= 11:
+    sifat = "Basa Lemah"
+else:
+    sifat = "Basa Kuat"
 
         st.success(f"pH = {ph} ({sifat})")
