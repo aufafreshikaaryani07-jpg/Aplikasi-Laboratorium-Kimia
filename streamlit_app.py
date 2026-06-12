4f18527d0fd17970d5fa0f3876c1ced686954bf2
aufafreshikaaryani07-jpg <aufafreshikaaryani07@gmail.com>
2026-06-12 19:22:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -153,7 +153,33 @@ if menu == "Home":
 
     </div>
     """, unsafe_allow_html=True)
+st.markdown("""
+<div style="
+background:linear-gradient(135deg,#001845,#023e8a,#0077b6);
+padding:30px;
+border-radius:20px;
+color:white;
+text-align:center;
+margin-top:20px;
+">
 
+<h2>👨‍🔬 Creator MOLEVIA</h2>
+
+<h4>Politeknik AKA Bogor</h4>
+<h4>Kelompok 5 - Kelas 1D</h4>
+
+<hr>
+
+<p><b>Aufa Freshika Aryani</b><br>NIM : 2560588</p>
+
+<p><b>Aura Halimah Natanegoro</b><br>NIM : 2560589</p>
+
+<p><b>Ayu Asyfa Mei Asyhari</b><br>NIM : 2560593</p>
+
+<p><b>Oscar Tirta Sugema</b><br>NIM : 2560735</p>
+
+</div>
+""", unsafe_allow_html=True)
     st.divider()
 # =====================================================
 # DATABASE ALAT LAB
