b47a8bb3f1595927fb222d2e314fa19877ce2d03
aufafreshikaaryani07-jpg <aufafreshikaaryani07@gmail.com>
2026-06-12 19:32:16 +0700

    Fix indentation in menu condition for 'Tentang Creator'

--- streamlit_app.py
+++ streamlit_app.py
@@ -463,7 +463,7 @@ elif menu == "Kalkulator pH":
         except:
             st.error("Masukkan angka yang valid! (contoh: 10^-4 atau 0.0001)")
 
-     elif menu == "Tentang Creator"
+elif menu == "Tentang Creator"
             st.markdown("""
 <div style="
 background:linear-gradient(135deg,#001845,#023e8a,#0077b6);
